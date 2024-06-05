6644916e559cd3db9f70d8c993c57d18cdc7dffb
Patryk Smacki <patryksmacki@Patryks-MacBook-Pro-9.local>
2024-06-05 15:16:30 +0100

    error in wsgi.py file

--- sssa_project/sssa_project/wsgi.py
+++ sssa_project/sssa_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sssa_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sssa_project.sssa_project.settings')
 
 application = get_wsgi_application()
